37e51c897f3a9773271f2a41dc85bf40991948ab
MilleGrinder <s224965@dtu.dk>
2025-11-11 20:26:19 +0100

    added a function for cleaning the data (selecting variables + pivot to long format)

---
+++ R/99_proj_func.R
@@ -0,0 +1,10 @@
+pivot_selected_variables <- function(data, ID_col, variables, tissue){
+  data |>
+    select(ID_col, ends_with(variables)) |>
+    pivot_longer(cols = ends_with(variables),
+                 names_to = c("cell_type", ".value"),
+                 names_pattern = "(.*)_(.*)",
+                 values_drop_na = TRUE) |>
+    mutate(tissue = tissue,
+           .before = ID_col)
+}
